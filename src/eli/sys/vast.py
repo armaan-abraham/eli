054f7ff384d8bc2e28614ae22002748f700436e5
Armaan Abraham <armaan.abraham@hotmail.com>
2025-05-13 18:55:23 +0000

    change vast script to delete

--- src/eli/sys/vast.py
+++ src/eli/sys/vast.py
@@ -1,20 +1,18 @@
 import os
 import requests
-import json
 
 
 vast_instance_id = os.environ["VAST_CONTAINERLABEL"].split("C.")[1]
-print(f"vast_instance_id: {vast_instance_id}")
 bearer_token = os.environ["VAST_BEARER_TOKEN"]
 
 payload = {}
-url = f"https://console.vast.ai/api/v0/instances/{vast_instance_id}/ssh/"
+url = f"https://console.vast.ai/api/v0/instances/{vast_instance_id}/"
 headers = {
    'Accept': 'application/json',
    'Content-Type': 'application/json',
    'Authorization': f'Bearer {bearer_token}'
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("DELETE", url, headers=headers, data=payload)
 
 print(response.text)
