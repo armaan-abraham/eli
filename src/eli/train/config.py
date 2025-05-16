ce0a3cf31c8df57661a153d589b022cb5883ccc4
Armaan Abraham <armaan.abraham@hotmail.com>
2025-05-16 17:43:07 +0000

    pythia-70m decoder test

--- src/eli/train/config.py
+++ src/eli/train/config.py
@@ -12,25 +12,25 @@ class TrainConfig:
     s3_bucket: str = "eli-datasets"
     dataset_name: str = "EleutherAI-pythia-70m-resid_post-4-5-100000000"
 
-    num_samples: int = int(1e7)
+    num_samples: int = int(2e7)
 
     seed: int = 42
 
     wandb_enabled: bool = True
 
-    dataset_loader_batch_size_samples: int = 64
+    dataset_loader_batch_size_samples: int = 256
     loss_control_batch_size_dataset_loader_batch_size_frac: float = 1.0
     dataset_loader_shuffle_buffer_size_wds_entries: int = 2
 
-    decoder_model_name: str = "gpt2"
-    decoder_model_embed_dim: int = 768
+    decoder_model_name: str = "EleutherAI/pythia-70m"
+    decoder_model_embed_dim: int = 512
 
     # Autocast
     _dtype_decoder: torch.dtype = torch.float16 
     _dtype_encoder: torch.dtype = torch.bfloat16
 
-    save_encoder_path: Optional[Path] = None
-    save_encoder_to_s3: bool = False
+    save_encoder_path: Optional[Path] = SAVE_DIR / "encoder.pt"
+    save_encoder_to_s3: bool = True
 
     log_loss_control_every_n_iter: int = 20
 
@@ -57,9 +57,9 @@ train_cfg = TrainConfig()
 class EncoderConfig:
     encoding_len_toks: int = 8
 
-    n_layers: int = 12
-    n_heads: int = 16
-    d_model: int = 1024
+    n_layers: int = 6
+    n_heads: int = 8
+    d_model: int = 768
     d_mlp_factor: int = 4
 
     lr: float = 2e-4
